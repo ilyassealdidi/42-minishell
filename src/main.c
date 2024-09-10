1920f2b4edb583428bf64525a7904acb178db4d1
Ilyasse <ilyassealdidi@gmail.com>
2024-09-11 00:37:31 +0100

    push

--- src/main.c
+++ src/main.c
@@ -6,70 +6,13 @@
 /*   By: ialdidi <ialdidi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:46:25 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/09/10 22:57:42 by ialdidi          ###   ########.fr       */
+/*   Updated: 2024/09/11 00:37:17 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
 int g_received_signal = 0;
-
-int	builtin(t_object *obj, t_list *node)
-{
-	t_command		*command;
-
-	command = node->content;
-	if (ft_strcmp(command->argv[0], EXIT) == 0)
-		return (builtin_exit(obj, command, true));
-	else if (ft_strcmp(command->argv[0], ECHO) == 0)
-		return (builtin_echo(command), SUCCESS);
-	else if (ft_strcmp(command->argv[0], EXPORT) == 0)
-		return (builtin_export(obj, command));
-	else if (ft_strcmp(command->argv[0], CD) == 0)
-		return (builtin_cd(obj, command));
-	else if (ft_strcmp(command->argv[0], PWD) == 0)
-		return (builtin_pwd(obj));
-	else if (ft_strcmp(command->argv[0], ENV) == 0)
-		return (builtin_env(obj), SUCCESS);
-	else
-		return (builtin_unset(obj, command));
-}
-
-int	execute_command(t_object *obj, t_list *node)
-{
-	t_command		*command;
-	int				status;
-
-	command = node->content;
-	status = SUCCESS;
-	if (command->cmd == NULL)
-	{
-		if (command->in != 0)
-			close(command->in);
-		if (command->out != 1)
-			close(command->out);
-	}
-	else if (is_builtin(command->argv[0]))
-		status = builtin(obj, node);
-	else
-		;// status = execve_command(obj, command);
-	//! Close the file descriptors
-	return (status);
-}
-
-int	execute_commands(t_object *obj)
-{
-	t_list			*commands;
-
-	commands = obj->commands;
-	while (commands)
-	{
-		if (execute_command(obj, commands) == FAILURE)
-			return (FAILURE);
-		commands = commands->next;
-	}
-	return (SUCCESS);
-}
 
 int	update_exit_status(t_object *obj)
 {
