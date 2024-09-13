231682b088f04910e41649e14b9ab11a2d50ab5c
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2024-09-13 10:01:56 +0100

    jh

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: ialdidi <ialdidi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:18:58 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/09/11 20:43:10 by ialdidi          ###   ########.fr       */
+/*   Updated: 2024/09/13 09:43:31 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,8 @@ int	generate_commands(t_object *obj)
 	if (commands_init(obj) == FAILURE || set_exit_status(obj) == FAILURE) // Why set_exit_status is here?
 		return (perror(EMBASE), ft_lstclear(&obj->tokens, destroy_token),
 			FAILURE);
-	ft_lstiter(obj->tokens, display_token);
+	// ft_lstiter(obj->tokens, display_token);
+	ft_lstiter(obj->commands, display_command);
 	ft_lstclear(&obj->tokens, destroy_token);
 	return (SUCCESS);
 }
