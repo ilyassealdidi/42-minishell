499ec53945814d53808818c0c59df11db215b9f4
Ilyasse <ilyassealdidi@gmail.com>
2024-09-15 23:10:18 +0100

    Include Exansion in heredog

---
+++ src/parse/utils/filename_generator.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filename_generator.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialdidi <ialdidi@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 22:34:30 by ialdidi           #+#    #+#             */
+/*   Updated: 2024/09/15 22:34:50 by ialdidi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+static char	*generate_filename(void)
+{
+	static int		i;
+	char			*number;
+	char			*name;
+
+	number = ft_itoa(i);
+	if (number == NULL)
+		return (i = 0, NULL);
+	name = ft_strjoin("/tmp/heredoc_", number);
+	free(number);
+	if (name == NULL)
+		return (i = 0, NULL);
+	i++;
+	return (name);
+}
