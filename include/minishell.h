9fa6567d445376c7d427fbb607adf3c212ffe03d
Ilyasse <ilyassealdidi@gmail.com>
2024-09-23 19:22:16 +0100

    setup for push

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ialdidi <ialdidi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:46:21 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/09/23 15:48:16 by aaitelka         ###   ########.fr       */
+/*   Updated: 2024/09/23 19:16:47 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <exec.h>
-// # include <debug.h> //! To be removed
 # include <parse.h>
 
 extern int		g_received_signal;
