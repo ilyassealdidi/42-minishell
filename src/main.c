10500841146b1c5f41bbdb5bf6d42894ce8faa77
Abdelhalim Aitelkadi <aaitelka@student.1337.ma>
2024-09-23 17:58:39 +0100

    exit status fixed when permission denied

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialdidi <ialdidi@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:46:25 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/09/22 12:45:02 by ialdidi          ###   ########.fr       */
+/*   Updated: 2024/09/23 16:49:52 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int argc, char **argv, char **env)
 	t_object		obj;
 	struct termios	term;
 
-	if (argc != 1 || !isatty(STDIN_FILENO))
-		return (ft_dprintf(2, "Usage: %s\n", argv[0]), EXIT_FAILURE);
+	// if (argc != 1 || !isatty(STDIN_FILENO))
+		// return (ft_dprintf(2, "Usage: %s\n", argv[0]), EXIT_FAILURE);
 	obj = (t_object){0};
 	init_signals();
 	if (init_env(&obj.env, env) == FAILURE)
